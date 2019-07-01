82d317fb0c4fd66056591bcf18d28a495a37d27a
Will Bush <williamdcbush@gmail.com>
2019-07-01 13:10:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.sort
-  puts array[-1]
+  array.max_by do |element|
+  element.field
+end
 end
 
 def find_min_value(array)
